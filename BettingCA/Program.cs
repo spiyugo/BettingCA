1b03064a7860df79404ba9219c99b5c22bdbe481
GORAN\Goran <spiyugo@gmail.com>
2017-05-31 23:36:31 +0200

    Initiated project.

---
+++ BettingCA/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace BettingCA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ReadData instance = new ReadData();
+            //System.Console.WriteLine("Hej");
+            instance.ReadTeamNames();
+            System.Console.ReadKey();
+
+        }
+    }
+}
---
+++ BettingCA/ReadData.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualBasic.FileIO;
+
+namespace BettingCA
+{
+    public class ReadData
+    {
+        public void ReadTeamNames()
+        {
+            using (TextFieldParser parser = new TextFieldParser(@"C:\Users\Goran\Documents\Visual Studio 2017\Projects\BettingCA\Data\test.csv"))
+            {
+                parser.TextFieldType = FieldType.Delimited;
+                parser.SetDelimiters(",");
+                while (!parser.EndOfData)
+                {
+                    //Processing row
+                    string[] fields = parser.ReadFields();
+                    int lastElement = fields.Length;
+                    int count = 0;
+                    foreach (string i in fields)
+                    {
+                        if (count < lastElement - 1)
+                        {
+                            System.Console.Write(i);
+                        }
+                        else
+                        {
+                            System.Console.WriteLine(i);
+                        }
+                        count++;
+                    }
+                    foreach (string field in fields)
+                    {
+                        //TODO: Process field
+                        System.Console.WriteLine(field);
+                    }
+                }
+            }
+        }
+        
+    }
+}
